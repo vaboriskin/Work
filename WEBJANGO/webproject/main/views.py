383d2768b505c1baec789ebe5d82c7dc5ae86c5b
Vitalik Boriskin <gkeyfrost-11@yandex.ru>
2024-10-08 02:03:22 +0300

    Django example

---
+++ WEBJANGO/webproject/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': '20',
+            'hobby': 'Hockey'
+        }
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
+
